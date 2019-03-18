85a4be3e2f5183759523d6c7c0fde88d4c02229c
vale75020 <valentinosuma@gmail.com>
2019-03-18 12:23:43 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Switch, Route } from 'react-router-dom'
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"; //import bootstrap from node modules
-import { spawn } from "child_process";
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Cart from './components/Cart'
+import Default from './components/Default'
+import Details from './components/Details'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <React.Fragment>  {/* html tag instead a <div></div> */}
+        <Navbar /> {/* navbar always present*/}
+        <Switch>
+          <Route exact path="/" component={ProductList} /> {/* exact pour eviter que le match sur "/" fasse areter le routing  */}
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
+
+
+ {/* pour tester bootstrap et fonts
+
+ <div className="container">
         <div className="row">
         <div className="col-6">column number one</div>
         <div className="col-6">
@@ -16,9 +39,4 @@ class App extends Component {
           </span>
           </div>
         </div>
-      </div>
-    );
-  }
-}
-
-export default App;
+      </div> */}
