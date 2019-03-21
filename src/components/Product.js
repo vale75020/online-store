5586a2e277e2c8e8f57f47ce3f3149a0e80bd3a2
vale75020 <valentinosuma@gmail.com>
2019-03-21 10:37:21 +0100

    cart card button

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
-import {productConsumer} from "../context"
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { productConsumer } from "../context";
 
-const ProductWrapper = styled.div`
-
-`
-
+const ProductWrapper = styled.div``;
 
 export default class Product extends Component {
   render() {
-    const {id, title, img, price, inCart} = this.props.product; //props from product de productList
+    const { id, title, img, price, inCart } = this.props.product; //props from product de productList
     return (
-      <ProductWrapper>
-        <h3> hello from Product</h3>
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        {/* <h3> hello from Product</h3> */}
+        <div className="card">
+          <div className="img-container p-5" onClick={console.log("ciao")}>
+            <Link to="/details">
+              <img src={img} alt="product" className="card-img-top" />
+            </Link>
+            <button className="cart-btn" disabled={inCart ? true : false}>
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  in inCart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
+          </div>
+        </div>
       </ProductWrapper>
     );
   }
